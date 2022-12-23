a124589c03b66d69b8298793c3dd13ce8a4f2bd1
Mikhail Andreev <potapich@yandex.ru>
2022-12-23 17:32:56 +0300

    page_making_an_order

--- pages/page_object_who_is_the_scooter_for.py
+++ pages/page_object_who_is_the_scooter_for.py
@@ -6,18 +6,21 @@ from selenium.webdriver.support.wait import WebDriverWait
 from locators.locators_for_price import LоcatorsDropDownList
 from locators.locators_who_is_the_scooter_for import LocatorsWhoIsTheScooterFor
 
+
 class WhoIsTheScooterFor:
     def __init__(self, driver):
         self.driver = driver
 
     def waiting_for_the_main_page_to_load(self):
-        WebDriverWait(self.driver, 12).until(expected_conditions.visibility_of_element_located((LоcatorsDropDownList.WAITING_FOR_THE_MAIN_PAGE)))
+        WebDriverWait(self.driver, 12).until(
+            expected_conditions.visibility_of_element_located((LоcatorsDropDownList.WAITING_FOR_THE_MAIN_PAGE)))
 
     def click_button_top_order(self):
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.BUTTON_TOP_ORDER).click()
 
     def waiting_title_who_is_the_scooter_for(self):
-        WebDriverWait(self.driver, 12).until(expected_conditions.visibility_of_element_located((LocatorsWhoIsTheScooterFor.TITLE_FOR_WHOM_IS_THE_SCOOTER)))
+        WebDriverWait(self.driver, 12).until(expected_conditions.visibility_of_element_located(
+            (LocatorsWhoIsTheScooterFor.TITLE_FOR_WHOM_IS_THE_SCOOTER)))
 
     def click_button_cookies(self):
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.CLICK_BUTTON_COOKIES).click()
@@ -35,13 +38,17 @@ class WhoIsTheScooterFor:
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.METRO_STATION_FIELD).send_keys(metro_station)
 
     def waiting_metro_station(self, metro_station):
-        WebDriverWait(self.driver, 12).until(expected_conditions.text_to_be_present_in_element_value((LocatorsWhoIsTheScooterFor.MEANING_METRO_STATION, metro_station)))
+        WebDriverWait(self.driver, 12).until(expected_conditions.text_to_be_present_in_element_value(
+            (LocatorsWhoIsTheScooterFor.MEANING_METRO_STATION, metro_station)))
 
     def click_metro_station_field(self):
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.METRO_STATION_FIELD).click()
 
     def click_on_the_list(self):
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.CLICK_ON_THE_LIST).click()
+
+    def click_on_the_list_two(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.CLICK_ON_THE_LIST_TWO).click()
 
     def fill_phone_field(self, phone):
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.PHONE_FIELD).send_keys(phone)
@@ -50,7 +57,8 @@ class WhoIsTheScooterFor:
         self.driver.find_element(*LocatorsWhoIsTheScooterFor.BUTTON_THE_NEXT).click()
 
     def waiting_title_about_rent(self):
-        WebDriverWait(self.driver, 12).until(expected_conditions.visibility_of_element_located((LocatorsWhoIsTheScooterFor.TITLE_ABOUT_RENT)))
+        WebDriverWait(self.driver, 12).until(
+            expected_conditions.visibility_of_element_located((LocatorsWhoIsTheScooterFor.TITLE_ABOUT_RENT)))
 
     def text_to_title_about_rent(self):
         return self.driver.find_element(*LocatorsWhoIsTheScooterFor.TITLE_ABOUT_RENT).text
@@ -65,3 +73,52 @@ class WhoIsTheScooterFor:
         self.fill_phone_field(phone)
         self.click_button_the_next()
 
+    def fill_field_who_two(self, name, last_name, address, metro_station, phone):
+        self.click_metro_station_field()
+        self.fill_metro_station_field(metro_station)
+        self.click_on_the_list_two()
+        self.fill_name_field(name)
+        self.fill_last_name_field(last_name)
+        self.fill_address_field(address)
+        self.fill_phone_field(phone)
+        self.click_button_the_next()
+
+    def click_when_field(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.WHEN_FIELD).click()
+
+    def fill_when_field(self, date):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.WHEN_FIELD).send_keys(date)
+
+    def click_to_an_empty_place(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.TO_AN_EMPTY_PLAСE).click()
+
+    def click_rental_period_field(self, ):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.RENTAL_PERIOD_FIELD).click()
+
+    def click_day_value(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.DAY_VALUE).click()
+
+    def click_scooter_color(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.SCOOTER_COLOR_FIELD).click()
+
+    def fill_comment_field(self, comment):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.COMMENT_FIELD).send_keys(comment)
+
+    def click_button_order(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.BUTTON_ORDER).click()
+
+    def click_button_yes(self):
+        self.driver.find_element(*LocatorsWhoIsTheScooterFor.BUTTON_YES).click()
+
+    def display_button_view_status(self):
+        return self.driver.find_element(*LocatorsWhoIsTheScooterFor.BUTTON_VIEW_STATUS).text
+
+    def fill_field_about_rent(self, date, comment):
+        self.fill_when_field(date)
+        self.click_to_an_empty_place()
+        self.click_rental_period_field()
+        self.click_day_value()
+        self.click_scooter_color()
+        self.fill_comment_field(comment)
+        self.click_button_order()
+        self.click_button_yes()
--- tests/test_who_is_the_scooter_for.py
+++ tests/test_who_is_the_scooter_for.py
@@ -1,7 +1,8 @@
 from pages.page_object_who_is_the_scooter_for import WhoIsTheScooterFor
 
+
 class TestWhoIsTheScooterFor:
-    def test_fill_field_who(self, driver):
+    def test_making_an_order_one(self, driver):
         driver.get('https://qa-scooter.praktikum-services.ru/')
 
         who_is_the_page = WhoIsTheScooterFor(driver)
@@ -13,13 +14,41 @@ class TestWhoIsTheScooterFor:
         phone = "+7777777777"
 
         who_is_the_page.waiting_for_the_main_page_to_load()
-
         who_is_the_page.click_button_top_order()
-
         who_is_the_page.click_button_cookies()
-
         who_is_the_page.fill_field_who(name, last_name, address, metro_station, phone)
 
-        text_to_title_about_rent = "Про аренду"
+        date = "20.12.2022"
+        comment = "Спасибо"
 
-        assert who_is_the_page.text_to_title_about_rent() == text_to_title_about_rent
+        who_is_the_page.fill_field_about_rent(date, comment)
+
+        button_text = who_is_the_page.display_button_view_status()
+        text = "Посмотреть статус"
+        assert button_text == text
+
+    def test_making_an_order_two(self, driver):
+        driver.get('https://qa-scooter.praktikum-services.ru/')
+
+        who_is_the_page = WhoIsTheScooterFor(driver)
+
+        name = "Игорь"
+        last_name = "Гусев"
+        address = "Москва"
+        metro_station = "Сокольники"
+        phone = "+7777777555"
+
+        who_is_the_page.waiting_for_the_main_page_to_load()
+        who_is_the_page.click_button_top_order()
+        who_is_the_page.click_button_cookies()
+        who_is_the_page.fill_field_who_two(name, last_name, address, metro_station, phone)
+
+        date = "21.12.2022"
+        comment = "Жду"
+
+        who_is_the_page.fill_field_about_rent(date, comment)
+
+        button_text = who_is_the_page.display_button_view_status()
+        text = "Посмотреть статус"
+        assert button_text == text
+
